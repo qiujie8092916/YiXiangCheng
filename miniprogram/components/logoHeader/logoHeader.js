dfdb850c950b54a6b7dda2c2f3eced493561f182
qiujie <qiujieee_empty@outlook.com>
2020-07-21 00:47:46 +0800

    :art: code -- 抽离logo-header

---
+++ miniprogram/components/logoHeader/logoHeader.js
@@ -0,0 +1,22 @@
+// components/logoHeader/logoHeader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    isShowOrder: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {},
+});
