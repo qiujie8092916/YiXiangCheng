5176f84e7b1bc5a5cedc8632f4607be217716dba
lampardme <lampardme@163.com>
2020-07-24 22:56:49 +0800

    :sparkles: feat - 地址组件更新

--- miniprogram/components/poiPicker/poiPicker.js
+++ miniprogram/components/poiPicker/poiPicker.js
@@ -1,44 +1,43 @@
 // components/poiPicker/poiPicker.js
 import { poiConfig } from "../../config";
-const chooseLocation = requirePlugin('chooseLocation');
+const chooseLocation = requirePlugin("chooseLocation");
 
 Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-    poi: {
-      type: String,
-      value: '请选择地址'
-    }
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    poi: "您在哪儿出发？",
   },
 
   lifetimes: {
     // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
-    attached: function () { },
-    moved: function () { },
-    detached: function () { },
+    attached: function () {},
+    moved: function () {},
+    detached: function () {},
   },
 
   // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
-  attached: function () { }, // 此处attached的声明会被lifetimes字段中的声明覆盖
-  ready: function() { },
+  attached: function () {}, // 此处attached的声明会被lifetimes字段中的声明覆盖
+  ready: function () {},
 
   pageLifetimes: {
     // 组件所在页面的生命周期函数
-    show: function () { 
+    show: function () {
       const location = chooseLocation.getLocation();
-      this.triggerEvent('choosePoi', location)
+      location &&
+        this.setData({
+          poi: location.name,
+        });
+      this.triggerEvent("choosePoi", location);
     },
-    hide: function () { },
-    resize: function () { },
+    hide: function () {},
+    resize: function () {},
   },
 
   /**
@@ -51,8 +50,8 @@ Component({
       const referer = poiConfig.referer;
 
       wx.navigateTo({
-        url: `plugin://chooseLocation/index?key=${key}&referer=${referer}`
+        url: `plugin://chooseLocation/index?key=${key}&referer=${referer}`,
       });
-    }
-  }
-})
+    },
+  },
+});
