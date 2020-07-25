d9ef3f1a6904f721e552556eb39fb2bf0f6fc64b
qiujie <qiujieee_empty@outlook.com>
2020-07-25 19:36:21 +0800

    :sparkles: feat -- logo-header样式微调，跳转订单列表，角标显示

--- miniprogram/components/logoHeader/logoHeader.js
+++ miniprogram/components/logoHeader/logoHeader.js
@@ -13,10 +13,22 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    orderCount: 0,
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    queryNonCompleteOrderCount() {
+      //TODO 获取未完单订单数量
+    },
+
+    goOrderList() {
+      wx.navigateTo({
+        url: "/pages/orderList/orderList",
+      });
+    },
+  },
 });
