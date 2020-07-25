f852d646066b829e21362657066c6fd55d94d0c2
qiujie <qiujieee_empty@outlook.com>
2020-07-25 19:43:46 +0800

    :sparkles: feat -- 引入wxsdk

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,5 +1,9 @@
 // 地图选点配置
 export const poiConfig = {
-  key: 'DSEBZ-F4PLU-VCSVS-4XIYK-JRFMQ-UKF6T',
-  referer: '享易程-打车'
-}
+  key: "DSEBZ-F4PLU-VCSVS-4XIYK-JRFMQ-UKF6T",
+  referer: "享易程-打车",
+};
+
+export const routeConfig = {
+  key: "KTGBZ-5OBRK-ATTJX-AWI37-F4HZQ-VEFEL",
+};
