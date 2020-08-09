9fce99e4520cd2809faf049f05d497d8f5044c2c
lampardme <lampardme@163.com>
2020-08-09 17:52:31 +0800

    :sparkles: feat - 订单详情

--- miniprogram/components/orderItem/orderItem.js
+++ miniprogram/components/orderItem/orderItem.js
@@ -52,5 +52,9 @@ Component({
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    chooseCurOrder() {
+      this.triggerEvent("chooseOrder");
+    },
+  },
 });
--- miniprogram/pages/orderDetail/orderDetail.js
+++ miniprogram/pages/orderDetail/orderDetail.js
@@ -1,4 +1,4 @@
-// miniprogram/pages/orderList/orderList.js
+// miniprogram/pages/orderDetail/orderDetail.js
 Page({
   /**
    * 页面的初始数据
@@ -44,8 +44,4 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {},
-
-  chooseCurOrder() {
-    wx.navigateTo({ url: "/pages/orderDetail/orderDetail" });
-  },
 });
