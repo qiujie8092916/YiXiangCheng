069279d67508ba29dc2afc5cade1dc31977a756d
jieq <qiujieee_empty@outlook.com>
2020-08-11 02:43:02 +0800

    :sparkles: 注册页初始化和交互

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -1,9 +1,20 @@
 // miniprogram/pages/register/register.js
+import { arrayToJson } from "../../utils/ext";
+
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    phone: "",
+    name: "",
+    company_array: [],
+    is_protocol: false,
+    company_address: [],
+    company_index: [0, 0],
+    selected_company: false,
+    employment_certificate: "",
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -54,11 +65,93 @@ Page({
         isCompany: 1,
       },
       success: (res) => {
-        console.log(res.result);
+        const company_address = res.result.reduce((acc, cur) => {
+          const area = cur.addrInfo.area;
+          const existIndex = acc.findIndex((it) => it.id === area);
+
+          if (existIndex === -1) {
+            acc.push({
+              id: cur.addrInfo.area,
+              area: cur.addrInfo.area,
+              address: [cur.addrInfo],
+            });
+          } else {
+            acc[existIndex].address.push(cur.addrInfo);
+          }
+          return acc;
+        }, []);
+
+        this.setData({
+          company_address,
+          company_array: [
+            company_address.map((it) => it.area),
+            company_address[this.data.company_index[0]].address.map(
+              (it) => it.name
+            ),
+          ],
+        });
       },
       fail(e) {
         console.log(e);
       },
+    });
+  },
+
+  handlerName({ detail }) {
+    this.setData({
+      name: detail.value,
+    });
+  },
+
+  chooseImg() {
+    if (this.data.employment_certificate) {
+      wx.previewImage({
+        urls: [this.data.employment_certificate],
+      });
+    } else {
+      wx.chooseImage({
+        count: 1,
+        sizeType: "compressed",
+        success: ({ tempFilePaths, tempFiles }) => {
+          console.log(tempFilePaths, tempFiles);
+          this.setData({
+            employment_certificate: tempFilePaths[0],
+          });
+        },
+      });
+    }
+  },
+
+  checkboxChange() {
+    this.setData({
+      is_protocol: !this.data.is_protocol,
+    });
+  },
+
+  changeCompany({ detail }) {
+    const company_index = this.data.company_index;
+
+    company_index[detail.column] = detail.value;
+
+    const second_array = this.data.company_address[
+      company_index[0]
+    ].address.map((it) => it.name);
+
+    if (company_index[1] > second_array.length - 1) {
+      company_index[1] = second_array.length - 1;
+    }
+
+    this.setData({
+      company_index,
+      "company_array[1]": second_array,
+    });
+  },
+
+  submitCompay({ detail }) {
+    console.log(detail);
+    this.setData({
+      selected_company: true,
+      company_index: detail.value,
     });
   },
 });
