ca0f9b46c192b5d414f2cb16b5eb6ac76689cf3c
qiujie <qiujie@iskytrip.com>
2020-08-21 00:40:52 +0800

    :sparkles: 订单控制器细分

--- cloudfunctions/orderController/index.js
+++ cloudfunctions/orderController/index.js
@@ -4,26 +4,111 @@ const cloud = require("wx-server-sdk");
 cloud.init();
 
 const db = cloud.database();
+const ACTIONS = {
+  create: ['createCharterOrder', "createCommuteOrder"],
+  query: ['queryCharterOrder', "queryCommuteOrder"],
+  cancel: ['cancelCharterOrder', "cancelCommuteOrder"],
+}
 
-// 云开发支付 https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/open/pay/CloudPay.unifiedOrder.html
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
+/**
+ * @desc 创建包车订单
+ * @param playload
+ */
+const createCharterOrder = async (playload) => {
+  console.log('create charter')
+}
+
+/**
+ * @desc 创建通勤订单
+ * @param {object} playload
+ * @param {0|1} playload.type 0-回家 1-上班
+ * @param {0|1} playload.take 0-拼车 1-独享
+ * @param {Array<number>} playload.from 坐标[longitude, latitude]
+ * @param {Array<number>} playload.to 坐标[longitude, latitude]
+ * @param {string} playload.time '2020-08-21 00:23:04'
+ * @param {string|number} playload.price 实付金额
+ */
+const createCommuteOrder = async (playload) => {
+  console.log(playload)
+  await sleep(1000)
+  return {
+    resultCode: 0,
+    resultData: true,
+    errMsg: ''
+  }
+}
+
+/**
+ * @desc 查询包车订单
+ * @param playload
+ */
+const queryCharterOrder = async (playload) => {
+  console.log('query charter')
+}
+
+/**
+ * @desc 查询通勤订单
+ * @param {object} playload
+ * @param {string} playload.orderId
+ */
+const queryCommuteOrder = async (playload) => {
+  console.log('query commute')
+}
+
+/**
+ * @desc 取消包车订单
+ * @param playload
+ */
+const cancelCharterOrder = async (playload) => {
+  console.log('cancel charter')
+}
+
+/**
+ * @desc 取消通勤订单
+ * @param playload
+ */
+const cancelCommuteOrder = async (playload) => {
+  console.log('cancel commute')
+}
 
 // 云函数入口函数
 /**
  * @desc 订单
+ * @link 云开发支付 https://developers.weixin.qq.com/miniprogram/dev/wxcloud/reference-sdk-api/open/pay/CloudPay.unifiedOrder.html
  * @param {RequestAction} event
  * @typedef RequestAction
+ * @property {create|query|cancel} RequestAction.action create-创建订单 query-查询订单 cancel-取消订单
  * @property {1|2} RequestAction.bizType 1-包车 2-通勤
- * @returns {Promise<>}
+ * @returns {Promise<{resultCode: number, resultData: any, errMsg: string}>}
  */
-exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();
-  genOrderNumber(event.bizType)
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  };
+exports.main = async (event) => {
+  // genOrderNumber(event.bizType)
+  console.log(event);
+
+  const {action, bizType, ...playload} = event
+
+  if(!['create', 'query', 'cancel'].includes(action)) {
+    return {
+      resultCode: -1,
+      resultData: null,
+      errMsg: 'action参数有误【create|query|cancel】'
+    }
+  }
+
+  if(![1, 2].includes(bizType)) {
+    return {
+      resultCode: -2,
+      resultData: null,
+      errMsg: 'bizType参数有误【1-包车|2-通勤】'
+    }
+  }
+
+  const funcName = ACTIONS[action][bizType - 1]
+
+  // return await (new Function(`return ${funcName}(${JSON.stringify(playload)})`))()
+  return await eval(`${funcName}(${JSON.stringify(playload)})`)
 };
 
 /**
