5d9173f7bdd9565a9aad4995a0431d65911cc576
qiujie <qiujieee_empty@outlook.com>
2020-08-28 03:44:59 +0800

    :sparkles: 添加Promise.finally

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,9 @@
 //app.js
 App({
   onLaunch: function () {
+    const promiseFinally = require("./utils/promise.finally.polyfill");
+    promiseFinally.init();
+
     if (!wx.cloud) {
       console.error("请使用 2.2.3 或以上的基础库以使用云能力");
     } else {
---
+++ miniprogram/utils/promise.finally.polyfill.js
@@ -0,0 +1,30 @@
+/**
+ * @description In User Settings Edit
+ * @exports class
+ * @Author jieq
+ * @Date 2020-05-14 17:28:38
+ * @LastEditors jieq
+ * @LastEditTime 2020-05-14 17:29:51
+ */
+
+module.exports = {
+  init() {
+    if (Promise && !Promise.prototype.finally) {
+      Promise.prototype.finally = function (callback) {
+        var Promise = this.constructor;
+        return this.then(
+          function (value) {
+            Promise.resolve(callback()).then(function () {
+              return value;
+            });
+          },
+          function (reason) {
+            Promise.resolve(callback()).then(function () {
+              throw reason;
+            });
+          }
+        );
+      };
+    }
+  },
+};
