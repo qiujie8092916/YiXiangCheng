f29b623182bef11d5b4f3f75a6c3b4ab1ac060f0
qiujie <qiujieee_empty@outlook.com>
2020-09-04 02:08:40 +0800

    :sparkles: 订单列表onshow刷新

--- miniprogram/pages/orderList/orderList.js
+++ miniprogram/pages/orderList/orderList.js
@@ -14,9 +14,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.init();
-  },
+  onLoad: function (options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -26,7 +24,9 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {},
+  onShow: function () {
+    this.init();
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
